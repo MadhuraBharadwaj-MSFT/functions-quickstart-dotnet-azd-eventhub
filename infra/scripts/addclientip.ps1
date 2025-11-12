f9387646dc31cf4cbf69329c51a54f7498037374
Thiago Almeida <thalme@microsoft.com>
2025-11-12 12:54:55 -0800

    Updating ip rule and EH firewall when in Vnet mode

--- infra/scripts/addclientip.ps1
+++ infra/scripts/addclientip.ps1
@@ -42,8 +42,11 @@ else {
     if ($false -eq $IPExists) {
         # Add the client IP to the network rule of the Event Hubs namespace
         Write-Output "Adding the client IP $ClientIP to the network rule of the Event Hubs service $EventHubNamespace"
-        az eventhubs namespace network-rule-set ip-rule add --resource-group $ResourceGroup --namespace-name $EventHubNamespace --ip-rule ip-address=$ClientIP/32 action=Allow > $null
+        az eventhubs namespace network-rule-set ip-rule add --resource-group $ResourceGroup --namespace-name $EventHubNamespace --ip-rule ip-address=$ClientIP action=Allow > $null
         
+        Write-Output "Setting Event Hubs network access to 'Selected networks' mode only"
+        az eventhubs namespace network-rule-set update --resource-group $ResourceGroup --name $EventHubNamespace --default-action "Deny" > $null
+
         # Mark the public network access as enabled since the client IP is added to the network rule
         $EventHubResourceId = az eventhubs namespace show --resource-group $ResourceGroup --name $EventHubNamespace --query id -o tsv
         az resource update --ids $EventHubResourceId --set properties.publicNetworkAccess="Enabled" > $null
